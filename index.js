d689e20308dd4b136eb53ae5bd5684cc921321e8
Ian Mugambi <ian.mugambi1@student.moringaschool.com>
2024-03-22 19:06:49 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// This is my code in this file!
+const currentUser = 'Ian Moe';
+
+//Welcome message
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//.slice()
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
